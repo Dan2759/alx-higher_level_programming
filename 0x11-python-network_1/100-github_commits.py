4898dea5146c01cb09fb7082e25c211ff2ead247
Dan2759 <kwizeradan.kd27@gmail.com>
2024-01-26 13:54:35 -0800

    python-network_1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Takes 2 arguments in order to solve this challenge.
+"""
+
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+    repo_name = argv[1]
+    owner_name = argv[2]
+    i = 0
+
+     URL = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
+
+    response = get(URL)
+    json = response.json()
+
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
