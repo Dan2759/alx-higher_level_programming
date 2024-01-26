f5baf30f94bf04f4fe468c2d8b2ac8120f298892
Dan2759 <kwizeradan.kd27@gmail.com>
2024-01-26 13:26:40 -0800

    python-network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,7 @@ from sys import argv
 from requests import post
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    email = sys.argv[2]
+    url = argv[1]
+    email = argv[2]
     res = post(url, {'email': email})
     print(res.text)
