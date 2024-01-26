747fdd6ef2db513d02f1b5c7ef00a3d2c49ae7a2
Dan2759 <kwizeradan.kd27@gmail.com>
2024-01-26 13:13:49 -0800

    python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,5 +10,5 @@ from sys import argv
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
+    response = get(url)
     print(response.headers.get('X-Request-Id'))
