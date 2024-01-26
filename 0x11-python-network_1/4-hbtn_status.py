1cd6675805299f6d8b8226bae04b38ed7f3c5a9a
Dan2759 <kwizeradan.kd27@gmail.com>
2024-01-26 13:03:39 -0800

    python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,9 +8,8 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     
-    r = requests.get(url)
-    x = r.text
-    body = response.read()
+    response = requests.get(url)
+
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format (response.text))
